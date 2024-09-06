f916dffcb1f0ac521b251c638b5974a44623d983
satyam pathak <satyampathak67@gmail.com>
2024-09-06 10:33:00 +0530

    0.0.1

--- scripts.js
+++ scripts.js
@@ -10,7 +10,4 @@
         }
     }
 
-    // When the user clicks on the button, scroll to the top of the document
-    function topFunction() {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    }
+ 
